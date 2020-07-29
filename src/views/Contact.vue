29afb82d3360f325b675d289980e76127483dbb0
jetk <jan.klug@gmail.com>
2020-07-29 12:07:00 +0100

    corrected yagni typo

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,7 +2,7 @@
  <div class="container">
      <h1>Contact Me</h1>
      <a href="https://www.linkedin.com/in/janklug/">Lazy, I know, but find me on LinkedIn</a>
-     <p>I'd love to build a bot-proof contact form for the sake of it but, you know, YAGNE. For now I'll just rely on LinkedIn to catch the worse of the spammers, instead of posting my private email address to get scraped.</p>
+     <p>I'd love to build a bot-proof contact form for the sake of it but, you know, YAGNI. For now I'll just rely on LinkedIn to catch the worse of the spammers, instead of posting my private email address to get scraped.</p>
      <h1>CV</h1>
      <a href="./cv/Jan_Klug_CV.pdf">Download my CV</a>
 </div>
